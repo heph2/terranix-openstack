e7f7db6f3353bc4256e3f34a7e6a4d7e224146ab
Heph <srht@mrkeebs.eu>
2022-04-22 18:19:11 +0200

    removed network module and reference in default.nix

--- default.nix
+++ default.nix
@@ -3,9 +3,6 @@
   imports = [
     ./module/server.nix
     ./module/provider.nix
-#    ./module/network_port.nix
-#    ./module/network_fip.nix
-#    ./module/network.nix
     ./module/export.nix
   ];
 }
