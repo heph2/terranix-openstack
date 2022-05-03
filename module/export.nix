e2dc15489472ab67c2c6e08471b383fc83d2f32b
Heph <srht@mrkeebs.eu>
2022-05-03 20:08:54 +0200

    fixed ip

--- module/export.nix
+++ module/export.nix
@@ -24,8 +24,8 @@ in {
                host = {
                  ipv4 = "''${ openstack_compute_instance_v2.${name}.access_ip_v4 }";
                  ipv6 = "''${ openstack_compute_instance_v2.${name}.access_ip_v6 }";
-                 ipv4-floating = "''${ openstack_compute_instance_v2.${name}.network/fixed_ip_v4 }";
-                 ipv6-floating = "''${ openstack_compute_instance_v2.${name}.network/fixed_ip_v6 }";
+                 ipv4-floating = "''${ openstack_networking_floatingip_v2.${name}-fip.fixed_ip_v4 }";
+                 ipv6-floating = "''${ openstack_networking_floatingip_v2.${name}-fip.fixed_ip_v6 }";
                };
                location = "''${ openstack_compute_instance_v2.${name}.region}";
              };
